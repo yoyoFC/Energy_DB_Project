0ba6157eedae3fd3a1b51821eb3458ac78d6a406
Joel Fernandez Cusi <88695570+yoyoFC@users.noreply.github.com>
2022-02-07 17:58:07 -0600

    Update config.py
    remove info

--- app/docs/config.py
+++ app/docs/config.py
@@ -1,8 +1,1 @@
-DB_HOST = 'dataviz.cgq2ewzuuqs1.us-east-2.rds.amazonaws.com'
-DB_USER = ''
-DB_PASS = ''
-DB_NAME = 'postgres'
-weather_api_key = "adef694c8be70b11b749f01ac3317177"
-g_key = "AIzaSyBxNhk8HFjqysMwKYMNH4rnKuJHd0QS8XM"
-api_key="AIzaSyDa799QehZ72cOrcB4U8B-o4I8-aCBq0vw"
-mapbox_access_token = "pk.eyJ1IjoicGtkaiIsImEiOiJja3l3YTNrZW4wNmZyMnBtd3lrM2Z3YWRmIn0.GQsKWwbdEUQI9kOQPAmmXA"
+
